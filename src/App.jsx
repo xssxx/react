8bb0f3eae6df63f4c9d1f40583ca906cd56f9e6d
xssxx <koonx6520@gmail.com>
2025-04-04 18:52:44 +0700

    remove unused

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import Navbar from "./components/Navbar";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import Product from "./pages/Product";
 
 function App() {
   return (
